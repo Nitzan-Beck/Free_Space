ad396c409575b38e742fac3e63df9e8897ec023e
Nitzan-Beck <s1494@motzkine.ort.org.il>
2020-12-28 16:18:59 +0200

    28.12.2020

--- app/src/main/java/com/example/free_space/SingIn.java
+++ app/src/main/java/com/example/free_space/SingIn.java
@@ -5,17 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SingIn extends AppCompatActivity {
-
+    private EditText name;
+    private EditText pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_in);
+        name=findViewById(R.id.editTextTxtUserName);
+        pass=findViewById(R.id.UserPassword);
     }
 
     public void btnBackToMainMenu(View view) {
         Intent i=new Intent(this,MainActivity.class);
         startActivity(i);
+    }
+
+    public void Send(View view) {
+        Toast.makeText(this,"Name: "+ name.getText()+", Password: "+ pass.getText(),Toast.LENGTH_LONG).show();
+
+
     }
 }
